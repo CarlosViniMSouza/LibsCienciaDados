75b43ef0669c4a931bdb50d36f63721ed57c5591
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-07-05 12:16:24 -0300

    Ex2 Matplotlib testing

--- ProcessamentoDados/Matplotlib/Horiz_Bar_Chart.py
+++ ProcessamentoDados/Matplotlib/Horiz_Bar_Chart.py
@@ -1,2 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+# Fixing random state for reproducibility
+np.random.seed(19587802)
+
+# Example data
+people = ('Patrick', 'Dick', 'Harry', 'Robert', 'Jimmy')
+y_pos = np.arange(len(people))
+performance = 5 + 10 * np.random.rand(len(people))
+error = np.random.rand(len(people))
+
+# Reading Collecting
+fig, ax = plt.subplots()
+
+h_bars = ax.barh(y_pos, performance, xerr = error, align = 'center')
+ax.set_yticks(y_pos)
+ax.set_yticklabels(people)
+ax.invert_yaxis()  # labels read top-to-bottom
+ax.set_xlabel('Performance')
+ax.set_title('How fast do you want to go moment?!')
+
+# Label with specially formatted floats
+ax.bar_label(h_bars, fmt='%.2f')
+ax.set_xlim(right=15)  # adjust xlim to fit labels
+plt.show()

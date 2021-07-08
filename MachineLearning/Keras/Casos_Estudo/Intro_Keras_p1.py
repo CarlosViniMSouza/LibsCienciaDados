665e7380bb6bc9ad73bcdd1bb1ab51609d3b00dd
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-07-07 22:09:43 -0300

    keras install

---
+++ MachineLearning/Keras/Casos_Estudo/Intro_Keras_p1.py
@@ -0,0 +1,19 @@
+# Required Libraries
+
+# Data manipulation
+import pandas as pd
+
+# Neural Network
+from tensorflow import keras
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.optimizers import RMSprop
+
+# Plot
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+# Assessment
+from sklearn.metrics import classification_report, confusion_matrix
+
